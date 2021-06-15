dbd5e335c64e9ad694bab7bbd867625ff0cd16dc
pjdiebold <85963127+pjdiebold@users.noreply.github.com>
2021-06-15 15:43:56 -0400

    Add files via upload

---
+++ cluster_assighn_tax/MOTHUR_classify_unfiltered.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WRK=/workdir/users/pd378/oilPCR/bulk_process_all_OIL_for_paper   #UPDATE THE WORKDIR
+FASTAS_ALL=$WRK/s6_cluster_otu/unfiltered_uniqs/clustered_otus.fa
+SILVA=/workdir/users/pd378/DataBases/MOTHUR_SILVA/silva.nr_v132.V4_oil
+
+cd $FASTAS
+
+/programs/mothur/mothur "#classify.seqs(fasta=$FASTAS_ALL,  template=$SILVA.align, taxonomy=$SILVA.tax, processors=10, cutoff=0)"
+
+
+#/programs/mothur/mothur "#classify.seqs(fasta=ALL_OTUs.fasta, template=$SILVA/silva.nr_v132.align, taxonomy=$SILVA/silva.nr_v132.tax, processors=20)"
